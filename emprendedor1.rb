a3158ec4ca8a9e365d271367f13c4d6855d59577
OBASTIAS <dj.osckaar@gmail.com>
2021-06-17 22:46:25 -0400

    revision final desafio

--- emprendedor1.rb
+++ emprendedor1.rb
@@ -1,14 +1,14 @@
-sales_prices = ARGV[0].to_i
-users = ARGV[1].to_i
-expenses = ARGV[2].to_i
-tax = 0.35
+sales_prices = ARGV[0].to_i #ingrese el precio de venta
+users = ARGV[1].to_i #ingrese el total de usuario del periodo (año)
+expenses = ARGV[2].to_i #ingrese los gastos del periodo (año)
+tax = 0.35 #impuesto a la renta del 35%
 
-utilidad = sales_prices * users - expenses
+utilidad = sales_prices * users - expenses #calculo de la utilidad
 taxes = (1 - tax).to_f
 
 if utilidad >0
     puts  "recuerde que el impuesto a este año es de un 35%"
     puts "#la utilidad despues de impuestos es de: #{utilidad * taxes}"
 else
-    puts  "No se esperan utilidades ya que el resultado del ejercicio es de: #{utilidad * taxes}"
+    puts  "No se esperan utilidades ya que el resultado del ejercicio es de: #{utilidad}"
 end
--- emprendedor2.rb
+++ emprendedor2.rb
@@ -1,16 +1,15 @@
-sales_prices = ARGV[0].to_i
-total_users = ARGV[2].to_i
-premium_users = ARGV[3].to_i
-free_users = ARGV[4].to_i
-expenses = ARGV[5].to_i
-tax = 0.35
-
-utilidad = sales_prices * users - expenses
+sales_prices = ARGV[0].to_i #ingresar el precio del producto.
+premium_users = ARGV[1].to_i #ingresar la cantidad de usuario premium.
+free_users = ARGV[2].to_i #ingresas la cantidad de usuarios gratuitos.
+expenses = ARGV[3].to_i #ingrese el gasto del periodo (año).
+total_users = premium_users + free_users #total de usuario (gratuitos + premium)
+tax = 0.35 #impuesto a la renta del 35%
+utilidad = sales_prices*2 * premium_users - expenses
 taxes = (1 - tax).to_f
 
-if utilidad >0
+if free_users >= 0 and utilidad <=  0
+    puts  "No se esperan utilidades ya que el resultado del ejercicio es de: #{utilidad}"
+else
     puts  "recuerde que el impuesto a este año es de un 35%"
     puts "#la utilidad despues de impuestos es de: #{utilidad * taxes}"
-else
-    puts  "No se esperan utilidades ya que el resultado del ejercicio es de: #{utilidad * taxes}"
 end
